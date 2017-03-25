4ca4fe8ea1ab4d16f41f00458e73206cbcd8abde
Josh Kaminsky <josh.kaminsky@gmail.com>
2017-03-24 20:39:01 -0400

    gitignore and formatting

--- app.go
+++ app.go
@@ -8,67 +8,68 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
 
-  "github.com/zmb3/spotify"
+	"github.com/zmb3/spotify"
 )
 
 // redirectURI is the OAuth redirect URI for the application.
 // You must register an application at Spotify's developer portal
 // and enter this value.
-const redirectURI = "http://192.168.100.10:8080/callback"
+const redirectURI = "http://crowdcast.app:8080/callback"
 
 var (
-  auth  = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate)
-  ch    = make(chan *spotify.Client)
-  state = "abc123"
+	auth  = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate)
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
 )
 
 func main() {
-  // first start an HTTP server
-  http.HandleFunc("/callback", completeAuth)
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    log.Println("Got request for:", r.URL.String())
-  })
-  go http.ListenAndServe(":8080", nil)
+	// first start an HTTP server
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
 
-  url := auth.AuthURL(state)
-  fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+	go http.ListenAndServe(":8080", nil)
 
-  // wait for auth to complete
-  client := <-ch
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
 
-  // use the client to make calls that require authorization
-  user, err := client.CurrentUser()
-  if err != nil {
-    log.Fatal(err)
-  }
+	// wait for auth to complete
+	client := <-ch
 
-  fmt.Println("You are logged in as:", user.ID)
+	// use the client to make calls that require authorization
+	user, err := client.CurrentUser()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  playlists, err := client.GetPlaylistsForUser(user.ID)
-  if err != nil {
-    log.Fatal(err)
-  }
+	fmt.Println("You are logged in as:", user.ID)
 
-  fmt.Println("Playlists:", playlists)
+	playlists, err := client.GetPlaylistsForUser(user.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Playlists:", playlists)
 
 }
 
 func completeAuth(w http.ResponseWriter, r *http.Request) {
-  tok, err := auth.Token(state, r)
-  if err != nil {
-    http.Error(w, "Couldn't get token", http.StatusForbidden)
-    log.Fatal(err)
-  }
-  if st := r.FormValue("state"); st != state {
-    http.NotFound(w, r)
-    log.Fatalf("State mismatch: %s != %s\n", st, state)
-  }
-  // use the token to get an authenticated client
-  client := auth.NewClient(tok)
-  fmt.Fprintf(w, "Login Completed!")
-  ch <- &client
+	tok, err := auth.Token(state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+	// use the token to get an authenticated client
+	client := auth.NewClient(tok)
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- &client
 }
